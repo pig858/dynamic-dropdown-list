06a6ee986db99df655c1ca467813915b0899767a
Max <pig85888@gmail.com>
2017-08-21 14:22:45 +0800

    Create actionAJAX.php

---
+++ actionAJAX.php
@@ -0,0 +1,14 @@
+<?php
+    header('Content-Type: application/json;charset=utf-8'); //return json string
+    include("common.inc.php");
+    $area_id = $_POST['area_id'];
+    $jsonArray = array();
+    if ($area_id != 0) {
+        /* do some stuff to get data(level 2)*/
+    } else {
+        echo 0;
+        return;
+    }
+    echo json_encode($jsonArray);
+    return;
+?>
