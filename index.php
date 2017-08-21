a2d70b36053e152a84f9dde173fd8004c236f539
Max <pig85888@gmail.com>
2017-08-21 14:24:53 +0800

    Update and rename html to index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <script src="//code.jquery.com/jquery-1.8.3.min.js"></script>
+    <script src="js/areaChange.js"></script>
+</head>
+
+<select id="areaSelect">
+    <option value="no" selected>Please select!</option>
+    <?php
+        /* do some stuff to show ur data(level 1) */
+    ?>
+</select>
+<select id="citySelect">
+      <option value="" selected>Please select!</option>
+</select>
+  <script>areaChange();</script> 
